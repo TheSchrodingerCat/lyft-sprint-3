5cdfb777441a97df94175f761e22a1b97a7c7601
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-05-24 11:36:22 -0400

    no funcionó la entrada de datos, por tanto no se pudo enlazar al juego

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,96 @@
+//------- para cambiar del input al tablero ------------------
+
+var start = document.getElementById("start-play");
+
+var coordenadas = document.getElementById("init-play");
+var area = document.getElementById("table-car");
+
+start.addEventListener("click",function(){
+	coordenadas.classList.toggle("nothing");
+	area.classList.toggle("nothing");
+});
+
+
+//constructor
+
+//---------- auto -------------------------------
+
+function Auto(posicion_x,posicion_y){
+	this.posicion_x = posicion_x;
+	this.posicion_y = posicion_y;
+	this.avanzar = function(){
+		this.posicion_y += 1;
+		console.log(this.posicion_y);
+	};
+	this.retroceder = function(){
+		this.posicion_y -= 1;
+		console.log(this.posicion_y);
+	};
+	this.ir_derecha = function(){
+		this.posicion_x += 1;
+		console.log(this.posicion_x);
+	};
+	this.ir_izquierda = function(){
+		this.posicion_x -= 1;
+		console.log(this.posicion_x);
+	}
+}
+
+//coordenadas iniciales del auto
+var coordX = document.getElementById("posicion-x").value;
+var coordY = document.getElementById("posicion-y").value;
+
+//luego la posicion del auto es
+
+//var posAuto = tablero[0][0];
+
+/*var numeros = [1,2,3,4,5,6,7,8,9,10];
+
+var x, y;
+for (i=0 ; i<10 ; i++){
+	if (numeros[i].toString() === coordX){
+		x = numeros[i];
+	}
+	if (numeros[i].toString() === coordY){
+		y = numeros[i];
+	}
+}*/
+
+
+
+//creamos nuestro auto
+
+var honda = new Auto(0,0);
+
+//ahora enlazamos con los eventos
+//rescatamos el código de las teclas con un constructor literal
+
+var tecla = {
+	LEFT: 37,
+	UP: 38,
+	RIGHT:39,
+	DOWN: 40
+}
+
+//funcion de eventos
+function way(event){
+   var evento = event.keyCode;
+   if (evento==tecla.UP/* && coordY<6*/){
+      honda.avanzar();
+   } else if (evento==tecla.LEFT/* && coordX>0*/){
+      honda.ir_izquierda();
+   } else if (evento==tecla.RIGHT/* && coordX<10*/){
+      honda.ir_derecha();
+   } else if (evento==tecla.DOWN/* && coordY>0*/){
+      honda.retroceder();
+   }
+}
+
+//todo esto se ejecutará cuando se presionen las teclas UP, DOWN, LEFT Y RIGHT
+
+document.addEventListener("keydown",way);
+
+
 
 //----------- tablero ----------------------
 
@@ -21,6 +114,7 @@ for (i=0 ; i<largeX ; i++){
 		//se muestra en el HTML
 		alto.innerHTML = tablero[i][j];
 		alto.setAttribute("class","altoCasilla");
+		alto.setAttribute("id","pos-" + i + "-" + j);
 
 		//cada ancho almacena largeY divs de alto, por tanto:
 		ancho.appendChild(alto);
@@ -28,78 +122,4 @@ for (i=0 ; i<largeX ; i++){
 	//ahora la div padre adopta el tablero
 	table.appendChild(ancho);
 }
-
-//coordenadas iniciales del auto
-var coordX = document.getElementById("posicion-x").value;
-var coordY = document.getElementById("posicion-y").value;
-
-//luego la posicion del auto es
-
-var posAuto = tablero[coordX][coordY];
-
-
-//---------- auto -------------------------------
-
-//constructor
-
-function Auto(posicion_x,posicion_y){
-	this.posicion_x = posicion_x;
-	this.posicion_y = posicion_y;
-	this.avanzar = function(){
-		posicion_y += 1;
-		console.log(posicion_y);
-		return posicion_y;
-	};
-	this.retroceder = function(){
-		posicion_y -= 1;
-		console.log(posicion_y);
-		return posicion_y;
-	};
-	this.ir_derecha = function(){
-		posicion_x += 1;
-		console.log(posicion_x);
-		return posicion_x;
-	};
-	this.ir_izquierda = function(){
-		posicion_x -= 1;
-		console.log(posicion_x);
-		return posicion_x;
-	}
-}
-
-
-//creamos nuestro auto
-
-var honda = new Auto(coordX,coordY);
-
-//ahora enlazamos con los eventos
-//rescatamos el código de las teclas con un constructor literal
-
-var tecla = {
-	LEFT: 37,
-	UP: 38,
-	RIGHT:39,
-	DOWN: 40
-}
-
-//funcion de eventos
-function way(event){
-   var evento = event.keyCode;
-   if (evento==tecla.UP && coordY<6){
-      honda.avanzar();
-   } else if (evento==tecla.LEFT && coordX>0){
-      honda.ir_izquierda();
-   } else if (evento==tecla.RIGHT && coordX<10){
-      honda.ir_derecha();
-   } else if (evento==tecla.DOWN && coordY>0){
-      honda.retroceder();
-   }
-}
-
-//todo esto se ejecutará cuando se presionen las teclas UP, DOWN, LEFT Y RIGHT
-
-document.addEventListener("keydown",way);
-
-
-
 
