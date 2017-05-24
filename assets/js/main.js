6e927a0f17a439069c7596fb2ec66846193b00b8
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-05-24 01:09:44 -0600

    constructor del auto con atributos y metodos listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,11 @@
 
-//tablero
+//----------- tablero ----------------------
 
 var tablero = [];
 for (i=0 ; i<6 ; i++){
 	tablero.push([0,0,0,0,0,0,0,0,0,0]);
 }
 
-//coordenadas iniciales del auto
-
-var coordX = document.getElementById("posicion-x").value;
-var coordY = document.getElementById("posicion-y").value;
 
 var table = document.getElementById("table-car");
 
@@ -35,6 +31,41 @@ for (i=0 ; i<largeX ; i++){
 }
 
 
+//---------- auto -------------------------------
+
+//constructor
+
+function Auto(posicion_x,posicion_y){
+	this.posicion_x = posicion_x;
+	this.posicion_y = posicion_y;
+	this.avanzar = function(){
+		posicion_y += 1;
+		return posicion_y;
+	};
+	this.retroceder = function(){
+		posicion_y -= 1;
+		return posicion_y;
+	};
+	this.ir_derecha = function(){
+		posicion_x += 1;
+		return posicion_x;
+	};
+	this.ir_izquierda = function(){
+		posicion_x -= 1;
+		return posicion_x;
+	}
+}
+
+//coordenadas iniciales del auto
+
+var coordX = document.getElementById("posicion-x").value;
+var coordY = document.getElementById("posicion-y").value;
+
+//creamos nuestro auto
+
+var honda = new Auto(coordX,coordY);
+
+//ahora enlazamos con los eventos
 
 
 
@@ -42,4 +73,5 @@ for (i=0 ; i<largeX ; i++){
 
 
 
-//auto
+
+
